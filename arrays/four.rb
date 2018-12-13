b5fa53e2a8f1a1ff2de62a9a7c4724a04545ff8c
md9000 <12439007+md9000@users.noreply.github.com>
2018-12-13 12:37:19 -0500

    update array questions

---
+++ arrays/four.rb
@@ -0,0 +1,17 @@
+#  What does each method return in the following example?
+
+arr = [15, 7, 18, 5, 12, 8, 5, 1]
+
+# This returns the array
+
+1. arr.index(5)
+
+# This returns 3, because that is the index of the first 5
+
+2. arr.index[5]
+
+# This returns an error
+
+3. arr[5]
+
+# This returns 8, because it is at index 5
---
+++ arrays/seven.rb
@@ -0,0 +1,10 @@
+arr = [1, 2, 3, 4, 5]
+
+
+arrnew = arr.map do |x|
+  x + 2
+end
+
+p arr
+p arrnewS
+
